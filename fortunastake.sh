8d8b26c20d26cd7515419acd37dfddfc568eba91
buzzkillb <travanx@gmail.com>
2019-02-05 20:25:13 -0800

    back to master branch

--- fortunastake.sh
+++ fortunastake.sh
@@ -44,7 +44,7 @@ sudo apt-get --assume-yes install git unzip build-essential libssl-dev libdb++-d
 echo "Installing Denarius Wallet"
 git clone https://github.com/carsenk/denarius
 cd denarius
-git checkout v3.4
+git checkout master
 git pull
 cd src
 make -f makefile.unix
